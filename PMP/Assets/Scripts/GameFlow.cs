bdc3d1dc90b9d1eb300b0f52ecedf256d9a7c7f5
Gabriel Hult <30988600+gabrielhult@users.noreply.github.com>
2022-09-13 15:53:56 +0200

    Making obstacles

--- PMP/Assets/Scripts/GameFlow.cs
+++ PMP/Assets/Scripts/GameFlow.cs
@@ -6,12 +6,16 @@ public class GameFlow : MonoBehaviour
 {
 
     public Transform firstMainTile;
-    public Transform obstacleObject;
+    public Transform[] obstacleObjects;
+    private int randObstacleIndex;
     public PlayerBehaviour playerBehaviour;
     private Vector3 nextMainTileSpawn;
     private Vector3 nextObstacleSpawn;
     private float currentVelocityX;
     private int randZObstacleIndex;
+    private int stepValue = 6;
+    private int roundValue;
+    private int adjustedValue;
 
 
 
@@ -28,15 +32,27 @@ public class GameFlow : MonoBehaviour
     {
         
     }
+
     IEnumerator spawnMainTile(){
+        //Logic for how often new tiles spawn.
         if(playerBehaviour.playerVelocity.x < 0.5){
             currentVelocityX = 1;
         }else currentVelocityX = playerBehaviour.playerVelocity.x;
         yield return new WaitForSeconds(1 / (currentVelocityX / 1.4f)); //Optimera detta
+
+        //Randomly places an obstacle
         randZObstacleIndex = Random.Range(-6, 7);
+        
+        //Rounds value so that obstacle spawns at the different roads
+        roundValue = ((int)Mathf.Floor(randZObstacleIndex / stepValue));
+        adjustedValue = stepValue * roundValue;
         nextObstacleSpawn = nextMainTileSpawn;
-        nextObstacleSpawn.z = randZObstacleIndex;
-        Instantiate(obstacleObject, nextObstacleSpawn, obstacleObject.rotation);
+        nextObstacleSpawn.z = adjustedValue;
+
+        //Randomly chooses an obstacle type from GameManager array
+        randObstacleIndex = Random.Range(0, obstacleObjects.Length);
+
+        Instantiate(obstacleObjects[randObstacleIndex], nextObstacleSpawn, obstacleObjects[randObstacleIndex].rotation);
         Instantiate(firstMainTile, nextMainTileSpawn, firstMainTile.rotation);
         nextMainTileSpawn.x += 4;
         StartCoroutine(spawnMainTile());
