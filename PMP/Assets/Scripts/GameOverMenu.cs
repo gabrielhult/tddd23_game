f728ceabf5b9997a5d79015e5a6d27ca4a0c114a
Gabriel Hult <30988600+gabrielhult@users.noreply.github.com>
2022-10-25 14:05:25 +0200

    1.0 submitted!!

--- PMP/Assets/Scripts/GameOverMenu.cs
+++ PMP/Assets/Scripts/GameOverMenu.cs
@@ -6,6 +6,7 @@ public class GameOverMenu : MonoBehaviour
 {
 
     public GameObject gameOverMenu;
+    public GameObject muteUI;
 
     // Update is called once per frame
     void Update()
@@ -13,5 +14,9 @@ public class GameOverMenu : MonoBehaviour
         if(GameManager.Instance.isGameOver){
             gameOverMenu.SetActive(true);
         }else gameOverMenu.SetActive(false);
+
+        if(MuteManager.Instance.isMuted){
+            muteUI.SetActive(true);
+        }else muteUI.SetActive(false);
     }
 }
